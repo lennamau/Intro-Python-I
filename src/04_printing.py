38783f7fafa38b46b17a497f9e14a877af442aba
Lenna <lenna@lennamau.com>
2019-09-02 21:39:52 -0700

    problem 5 completed

--- src/04_printing.py
+++ src/04_printing.py
@@ -12,6 +12,14 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print("x is %d, y is %.2f, z is \"%s\" " % (x, y, z))
+
 # Use the 'format' string method to print the same thing
 
+my_string = "x is {0}, y is {1:.2f}, z is \"{2}\"" 
+print(my_string.format(x, y, z))
+
 # Finally, print the same thing using an f-string
+
+print(f"x is {x}, y is {y:.2f}, z is \"{z}\"")
+
